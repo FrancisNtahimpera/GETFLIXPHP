1a9ed6713c09c334a744df25840a1b5b97b03dae
francisntahimpera <francisntahimpera@gmail.com>
2021-11-24 00:33:12 +0100

    modif index.php,inscription.php creation de head.php et footer.php

--- docker_env/application/source/index.php
+++ docker_env/application/source/index.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="Ie=Edge,chrome=1">
-  <title>Getflix SIGNIN</title>
-  <meta name="generator" content="MediaWiki 1.22.5">
-
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-</head>
+<?php require "head.php"; ?>
 <body>
 
   <h1>Getflix</h1>
--- docker_env/application/source/inscription.php
+++ docker_env/application/source/inscription.php
@@ -1,29 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Inscription</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="style.css">
-</head>
-
- 
+ <?php require "head.php"; ?>
   
 
 <body>
 
     <form class="container" id="containerInscription" action="">
+    <h1>Register</h1>
         <div class="row">
-            <h1>Register</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr>
             
-                <div class="col-md-6 col-sm-12">
+           
+            
+            
+                <div class="col-lg-6 col-md-6 col-sm-12">
               
                     <label for="first-name"><b>First Name</b></label>
                     <input type="text" placeholder="Enter your first name" name="first-name" id="first-name" required>
@@ -38,7 +29,7 @@
                     <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
                 </div>
                 
-                <div class="col-md-6 col-sm-12">
+                <div class="col-lg-6 col-md-6 col-sm-12">
                     
                     <label for="email"><b>Email</b></label>
                     <input type="text" placeholder="Enter Email" name="email" id="email" required>
@@ -50,8 +41,8 @@
                     <input type="text" placeholder="Enter birthday" name="birthday" id="birthday" required>
                 </div>
                 
-                <div class="row">
-                    <div class="container-fluid d-flex justify-content-center">
+                <div class="container " id="containerVisa">
+                    <div class="row container-fluid  justify-content-center" id="containerVisa">
                         <div class="col-sm-12 col-md-12">
                             <div class="card">
                                 <div class="card-header ">
@@ -60,7 +51,7 @@
                                         <div class="col-md-6 text-right" style="margin-top: -5px;"> <img src="https://img.icons8.com/color/36/000000/visa.png"> <img src="https://img.icons8.com/color/36/000000/mastercard.png">  </div>
                                     </div>
                                 </div>
-                                <div class="card-body" style="height: 350px">
+                                <div class="card-body col-lg-12" style="height: 350px">
                                     <div class="form-group"> <label for="cc-number" class="control-label">CARD NUMBER</label> 
                                         <input id="cc-number" type="tel" class="input-lg form-control cc-number" autocomplete="cc-number" placeholder="•••• •••• •••• ••••" pattern="^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$" required> </div>
                                     <div class="row">
@@ -78,15 +69,15 @@
                             </div>
                         </div>
                     </div>
+                    <div class="  container-fluid  col-lg-12 justify-content-center"></br>
+                        <div class="form-check">
+                                <input class="form-check-input " type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault"> I AGREE WITH THE TERMS <a href="url">Contract</a>     </label></br>
+                                <button type="submit" class="registerbtn d-grid gap-2 col-12 mx-auto" id="register">Register</button>
                 </div>
     
-            
-                <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-                <button type="submit" class="registerbtn" id="register">Register</button>
-           
-        
-            <div class="container signin">
-                <p>Already have an account? <a href="#">Sign in</a>.</p>
+            <div class="col-lg-12 ">
+                <p>Already have an account? <a href="index.php">Sign in</a>.</p>
             </div>
         </div>
         </div>
