57b0c2fbcc160902b50d6d986d69ec6ea9493fcd
borisbecode <boris.lorenzonetto@gmail.com>
2021-11-26 12:31:24 +0100

    test

--- docker_env/application/source/include/connection.php
+++ docker_env/application/source/include/connection.php
@@ -5,11 +5,10 @@ $username = "root";
 $password = "root";
 $dbname = "getflix";
 
-
+// test
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // check connection
 if ($conn->connect_error) {
     error_log("Connection failed: " . $conn->connect_error);
-} else 
-?>
+} else
